713f1a0e8e23a350df333dd94ce6a22455469577
lialex123 <57013544+lialex123@users.noreply.github.com>
2022-11-07 15:19:29 -0500

    Update MCTS.py

--- MCTS.py
+++ MCTS.py
@@ -71,7 +71,7 @@ class MCTS():
         instead of needing a rollout
 
         Returns:
-            self.Ps[state], val: the initial policy vector and value given by the neural network
+            r: the reward given by the neural network
         """
         self.Ps[state], val = self.nnet.predict(canonicalBoard)
         valids = self.game.getValidMoves(canonicalBoard, 1)
@@ -106,10 +106,10 @@ class MCTS():
 
     def nextState(self, canonicalBoard, action):
         """
-        Gets the next state given the action
+        Gets the next board state given the action
 
         Returns:
-            nextState: the next state given the action
+            nextBoard: the next board state given the action
         """
 
         nextState, nextPlayer = self.game.getNextState(canonicalBoard, 1, action)
@@ -153,22 +153,21 @@ class MCTS():
 
     def search(self, initial_board):
         """
-        This function performs one iteration of MCTS. It is recursively called
-        till a leaf node is found. The action chosen at each node is one that
+        This function performs MCTS. The action chosen at each node is one that
         has the maximum upper confidence bound as in the paper.
 
-        Once a leaf node is found, the neural network is called to return an
-        initial policy P and a value v for the state. This value is propagated
+        Once a leaf node is found, the neural network is called to return a
+        reward r for the state. This value is propagated
         up the search path. In case the leaf node is a terminal state, the
         outcome is propagated up the search path. The values of Ns, Nsa, Qsa are
         updated.
 
-        NOTE: the return values are the negative of the value of the current
-        state. This is done since v is in [-1,1] and if v is the value of a
-        state for the current player, then its value is -v for the other player.
+        NOTE: the return values are the negative of the reward of the current
+        state. This is done since r is in [-1,1] and if r is the value of a
+        state for the current player, then its value is -r for the other player.
 
         Returns:
-            v: the negative of the value of the current canonicalBoard
+            b0: the initial board state of the othello board
         """
         initial_state = self.game.stringRepresentation(initial_board)
 
